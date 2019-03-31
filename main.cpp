f3f0a0438336ab10f68590e93cddd078aff38985
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2019-03-31 00:18:48 -0500

    Added basic file reading for testing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
+#include <fstream>
 #include <iostream>
+#include <string>
 
 int main() {
-    std::cout << "cs3210-course-project" << std::endl;
+    // Open ./input/map.txt and write its contents to mapStr
+    std::string mapStr;
+    std::string mapIn;
+    std::fstream mapFile("../input/map.txt");
+    if (mapFile.is_open()) {
+        while (getline(mapFile, mapIn)) {
+            mapStr += mapIn + "\n";
+        }
+        mapFile.close();
+    } else {
+        std::cerr << "Error opening ./input/map.txt" << std::endl;
+        return -1;
+    }
+
+    // Open ./input/species.txt and write its contents to speciesStr
+    std::string speciesStr;
+    std::string speciesIn;
+    std::fstream speciesFile("../input/species.txt");
+    if (speciesFile.is_open()) {
+        while (getline(speciesFile, speciesIn)) {
+            speciesStr += speciesIn + "\n";
+        }
+        speciesFile.close();
+    } else {
+        std::cerr << "Error opening ./input/species.txt" << std::endl;
+        return -2;
+    }
+
     return 0;
 }
